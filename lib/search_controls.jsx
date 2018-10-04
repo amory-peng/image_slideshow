0e931544e38170ac59e72ee4e3b672c588e7d0a1
Amory Peng <amoryp@amazon.com>
2018-10-04 16:49:54 -0700

    fix comments

--- lib/search_controls.jsx
+++ lib/search_controls.jsx
@@ -1,1 +1,6 @@
 // component that talks with google api
+// search bar
+// submit
+// back and forth
+// NOTE: MAKE SURE U KEEP THE PREVIOUS RESULTS IN MEMORY. EXTRA QUERIES
+// WILL RATE LIMIT YOU FAST
